2efcb682fc54db2cbc95e61b8c787d2377f2246e
GiulioSpugnini <giuliospugnini@yahoo.com>
2021-12-22 15:10:55 +0100

    added steps to do

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log('JS OK!');
+
+/*
+1. Creo una variabile di appoggio
+2. Stampo i numeri da 1 a 100
+    2a. Per i multipli di 3 stampo 'fizz'
+    2b. Per i multipli di 5 stammpo 'buzz'
+    2c. Per i multipli di entrambi stampo 'fizzbuzz'
+
+*/
